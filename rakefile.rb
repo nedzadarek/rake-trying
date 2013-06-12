601b035b6828c16e3fad3ab6da9bb394fc112f6b
Darek Nedza <nedzadarek@gmail.com>
2013-06-12 14:37:20 +0200

    adds task that needs prerequisites

--- rakefile.rb
+++ rakefile.rb
@@ -3,3 +3,18 @@ desc 'first task'
 task :first do
 	puts 'This is my first task'
 end
+
+desc 'Prerequisite nr1'
+task :preq1 do
+	puts "I'm needed!"
+end
+
+desc 'Prerequisite nr2'
+task :preq2 do
+	puts "I'm needed too!"
+end
+
+desc 'Task that need preq1 and preq2'
+task :need_preqs => ["preq1", "preq2"] do
+	puts "I run every preqs"
+end
